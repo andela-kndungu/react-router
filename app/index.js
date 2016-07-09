204bee9ee3b776d48abe3c68acebdcf19f6660d7
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-09 09:47:19 +0300

    Lesson 2: Rendering a route

--- app/index.js
+++ app/index.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { render } from 'react-dom';
-import App from './components/App.jsx';
+import { Router, Route, hashHistory } from 'react-router';
 
-render(<App />, document.getElementById('app'));
+import About from './components/About.jsx';
+import App from './components/App.jsx';
+import Repos from './components/Repos.jsx';
+
+render(
+  <Router history={hashHistory}>
+    <Route path="/" component={App} />
+    <Route path="/about" component={About} />
+    <Route path="/repos" component={Repos} />
+  </Router>,
+  document.getElementById('app')
+);
 
