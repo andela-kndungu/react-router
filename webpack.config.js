968476db36abaaf2751025e3cc19faf73020283b
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-09 09:21:59 +0300

    Starting Point

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react'],
+      },
+    }],
+  },
+};
+
