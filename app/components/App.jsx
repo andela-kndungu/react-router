45566e720b8109855bd292d66184368e42c0ec6e
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-07-09 10:31:34 +0300

    Lesson 4: Nested Routes

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router';
 
-const App = () => {
+const App = (props) => {
   return (
     <div>
-      <h1>React Router Tutrorial</h1>
+      <h1>GhettoHub Issues</h1>
       <ul>
         <li><Link to="/about">About</Link></li>
         <li><Link to="/repos">Repos</Link></li>
       </ul>
+      {props.children}
     </div>
   );
 };
+
+App.propTypes = React.PropTypes.any;
 
 export default App;
 
--- app/index.js
+++ app/index.js
@@ -5,12 +5,12 @@ import { Router, Route, hashHistory } from 'react-router';
 import About from './components/About.jsx';
 import App from './components/App.jsx';
 import Repos from './components/Repos.jsx';
-
 render(
   <Router history={hashHistory}>
-    <Route path="/" component={App} />
-    <Route path="/about" component={About} />
-    <Route path="/repos" component={Repos} />
+    <Route path="/" component={App}>
+      <Route path="/about" component={About} />
+      <Route path="/repos" component={Repos} />
+    </Route>
   </Router>,
   document.getElementById('app')
 );
